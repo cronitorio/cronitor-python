254614c3551a073128f71361c46e2e2e4c6c73d2
Dharmendra Verma <dkverma.vit@gmail.com>
2017-04-20 19:39:59 +0530

    changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cronitor',
-    version='1.0',
+    version='1.0.1',
     packages=[],
     url='https://github.com/vy-labs/cronitor',
     license='MIT License',
