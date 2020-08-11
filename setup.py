faf6df937c2122948d387b57401419fbcfed124a
August Flanagan <august.flanagan@gmail.com>
2020-08-10 19:10:20 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='cronitor',
-    version='1.0.3',
+    version='2.0.0',
     packages=find_packages(),
-    url='https://github.com/vy-labs/cronitor',
+    url='https://github.com/cronitorio/cronitor-python',
     license='MIT License',
-    author='xrage',
+    author='aflanagan',
     author_email='',
-    description='Python wrapper for cronitor monitors',
+    description='Python wrapper for Cronitor\'s Monitor and Ping APIs.',
     install_requires=[
         'requests',
     ],
