0a7dff9830880d40cf5e6568b4a41d08a566cdff
August Flanagan <august.flanagan@gmail.com>
2020-08-21 10:34:40 -0700

    bump to 2.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cronitor',
-    version='2.0.0',
+    version='2.0.1',
     packages=find_packages(),
     url='https://github.com/cronitorio/cronitor-python',
     license='MIT License',
