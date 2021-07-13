22a875cc24dd75bc5b64fac90844a0a724f2a76b
August Flanagan <august.flanagan@gmail.com>
2021-07-13 13:46:44 -0700

    bump version to 4.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cronitor',
-    version='4.2.0',
+    version='4.2.1',
     packages=find_packages(),
     url='https://github.com/cronitorio/cronitor-python',
     license='MIT License',
