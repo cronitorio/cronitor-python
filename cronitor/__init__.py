62e5b9b2c475767eaf472d3884f3fba57c3d9943
August Flanagan <august.flanagan@gmail.com>
2022-08-23 16:52:54 -0700

    return boolean value from apply_config/validate_config

--- cronitor/__init__.py
+++ cronitor/__init__.py
@@ -18,7 +18,6 @@ api_version = os.getenv('CRONITOR_API_VERSION', None)
 environment = os.getenv('CRONITOR_ENVIRONMENT', None)
 config = os.getenv('CRONITOR_CONFIG', None)
 celerybeat_only = False
-default_env='production'
 
 # this is a pointer to the module object instance itself.
 this = sys.modules[__name__]
@@ -47,25 +46,25 @@ class State(object):
     FAIL = 'fail'
 
 # include_output is deprecated in favor of log_output and can be removed in 5.0 release
-def job(key, env=default_env, log_output=True, include_output=True):
+def job(key, env=None, log_output=True, include_output=True):
     def wrapper(func):
         @wraps(func)
         def wrapped(*args, **kwargs):
             start = datetime.now().timestamp()
 
-            monitor = Monitor(key)
+            monitor = Monitor(key, env=env)
             # use start as the series param to match run/fail/complete correctly
-            monitor.ping(state=State.RUN, series=start, env=env)
+            monitor.ping(state=State.RUN, series=start)
             try:
                 out = func(*args, **kwargs)
             except Exception as e:
                 duration = datetime.now().timestamp() - start
-                monitor.ping(state=State.FAIL, message=str(e), metrics={'duration': duration}, series=start, env=env)
+                monitor.ping(state=State.FAIL, message=str(e), metrics={'duration': duration}, series=start)
                 raise e
 
             duration = datetime.now().timestamp() - start
             message = str(out) if all([log_output, include_output]) else None
-            monitor.ping(state=State.COMPLETE, message=message, metrics={'duration': duration}, series=start, env=env)
+            monitor.ping(state=State.COMPLETE, message=message, metrics={'duration': duration}, series=start)
             return out
 
         return wrapped
@@ -91,8 +90,10 @@ def apply_config(rollback=False):
         heartbeat_count = len(monitors.get('heartbeats', []))
         total_count = sum([job_count, check_count, heartbeat_count])
         logger.info('{} monitor{} {}'.format(total_count, 's' if total_count != 1 else '', 'validated.' if rollback else 'synced.',))
+        return True
     except (yaml.YAMLError, ConfigValidationError, APIValidationError, APIError, AuthenticationError) as e:
         logger.error(e)
+        return False
 
 def read_config(path=None, output=False):
     this.config = path or this.config
